500b0f3d92dca80254cbef4775f1943f16d8ec61
Charlie Massry <charlesmassry@Charlies-MacBook-Pro.local>
2014-06-25 20:42:29 -0400

    added active record and post request

--- server.rb
+++ server.rb
@@ -1,22 +1,41 @@
 require "sinatra"
+require "active_record"
 
-GALLERIES = {
-  "cats" => ["grumpy_cat.png", "colonel_meow.jpg"],
-  "dogs" => ["shibe.png"],
-  "wombat" => []
-}
+ActiveRecord::Base.establish_connection(
+    adapter: 'postgresql',
+    database: 'pixtr'
+)
+
+class Image < ActiveRecord::Base
+end
+
+class Gallery < ActiveRecord::Base
+  has_many :images
+end
 
 get "/" do
-  @galleries = GALLERIES.keys
+  @galleries = Gallery.order("name ASC")
   erb :gallery, layout: :layout
 end
 
-get "/:name" do
-  @name = params[:name]
-  @filenames = GALLERIES[@name]
-  if GALLERIES.has_key?(@name)
+get "/galleries/new" do
+  erb :new_gallery, layout: :layout
+end
+
+get "/:gallery_name" do
+  @name = params[:gallery_name]
+  @name.capitalize!
+  @gallery = Gallery.find_by(name: @name)
+  if !@gallery.nil?
+    @images = @gallery.images
     erb :index, layout: :layout
   else
     erb :error, layout: :layout
   end
+end
+
+post '/galleries' do
+  gallery = Gallery.new(params[:gallery])
+  gallery.save
+  redirect to("/#{gallery.name}")
 end
